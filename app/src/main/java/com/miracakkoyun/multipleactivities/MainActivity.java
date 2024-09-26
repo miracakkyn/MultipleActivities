9df627d741f3e4e77e86b6ae80117a3a1ca4428b
Miraç Akkoyun <miracakkoyun00@gmail.com>
2024-09-27 02:18:32 +0300

    Multiple Activities

---
+++ app/src/main/java/com/miracakkoyun/multipleactivities/MainActivity.java
@@ -0,0 +1,41 @@
+package com.miracakkoyun.multipleactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    String userName="";
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        userName="";
+        editText=findViewById(R.id.editTextText);
+    }
+
+    public void changeActivity(View view){
+        userName=editText.getText().toString();
+        Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+        intent.putExtra("userInput",userName);
+        startActivity(intent);
+
+    }
+}
